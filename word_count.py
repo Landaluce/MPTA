c4cab95ab7247f65fc97a98682c53ca25cd35d88
Alvaro <delandaluce_alvaro@wheatoncollege.edu>
2017-02-14 13:27:31 -0500

    add docx to txt and find_words() (opportunity and threat)

--- word_count.py
+++ word_count.py
@@ -1,24 +1,17 @@
-from numpy import random
+import re
 from collections import Counter
 
-
-# For testing
-# return: a string with 2 character words
-def generate_random_string():
-    random.seed(1834)
-    string = ""
-    for j in range(100):
-        string += chr(random.randint(97, 122))
-        if j%2 == 0:
-            string += " "
-    return string
-
-
-# input:  string
-# return: Counter object
-def count_words(text):
-    return Counter(text.split())
-
+opportunity = ["advantage", "befalling", "break", "chance","connection",
+               "contingency", "convenience", "cut", "fair shake", "favorable",
+               "fighting chance", "fitness", "fling", "fortuity", "hope",
+               "luck", "recourse"]
+threat = ["alarming fateful", "apocalyptic", "baleful", "baneful", "black",
+          "cautionary", "challenging", "comminatory", "crisis", "dangerous",
+          "demur", "dire", "grim", "ill-boding", "imminent", "impendent",
+          "impending", "inauspicious", "intimidating", "looming", "loury",
+          "lowering", "lowery", "minacious", "minatory", "portending", "portent",
+          "sinister", "trial dire", "ugly", "unlucky", "unpropitious", "unsafe",
+          "warning",]
 
 
 # Delete useless words
@@ -31,21 +24,93 @@ def stop_words(counter):
             del counter[word]
     return counter
 
+# don't need it anymore
+def utf8_to_ascii(text):
+    text = text.replace(u'\u2014', '-')
+    text = text.replace(u'\u2013', '-')
+    exclude = set('!"#$%&()*+,./:;<=>?@[\]^_`{|}~')
+    exclude.add(u'\u2018')  # '
+    exclude.add(u'\u2019')  # '
+    exclude.add(u'\u201c')  # "
+    exclude.add(u'\u201d')  # "
+    exclude.add(u'\u2022')  # bullet point
+    exclude.add(u'\u2026')  # ...
+    exclude.add(u'\u00A5')
+
+    exclude.add(u'\u00F0')
+    exclude.add(u'\u00F1')
+    exclude.add(u'\u00F2')
+    exclude.add(u'\u00F3')
+    exclude.add(u'\u00F4')
+    exclude.add(u'\u00F5')
+
+    exclude.add(u'\u00A1')
+    exclude.add(u'\u00BC')
+    exclude.add(u'\u00A3')
+    exclude.add(u'\u00E9')
+    exclude.add(u'\u2021')
+    exclude.add(u'\u0161')
+    exclude.add(u'\u0142')
+    exclude.add(u'\u00BF')
+    exclude.add(u'\u2030')
+    exclude.add(u'\u2044')
+    exclude.add(u'\u00A8')
+    exclude.add(u'\u00A9')
+    exclude.add(u'\u00AA')
+    exclude.add(u'\u00FE')
+    exclude.add(u'\u00DF')
+    exclude.add(u'\u2122')
+    exclude.add(u'\u0153')
+    exclude.add(u'\uFB01')
+    exclude.add(u'\uFB02')
+    exclude.add(u'\u102A')
+    exclude.add(u'\u201A')
+    exclude.add(u'\u00AE')
+    exclude.add(u'\u00E7')
+    exclude.add(u'\u00F8')
+    exclude.add(u'\u00FC')
+    exclude.add(u'\u02DC')
+
+    text = ''.join(ch for ch in text if ch not in exclude)
+
+    return text
+
+
+def find_words(text):
+    op_count = 0
+    for word in opportunity:
+        op_count += len(re.findall(word, text))
+    th_count = 0
+    for word in threat:
+        th_count += len(re.findall(word, text))
+    tup = (th_count, op_count)
+    return tup
+
+
+def read_txt(filepath):
+    try:
+        with open(filepath, 'r') as myfile:
+            return myfile.read().decode("utf-8").replace('\n', '').lower()
+    except IOError:
+        print "could not read", filepath
+
+# test case
+def get_jpm_op_th():
+    print "JPM"
+    print "file, th, op"
+    for i in range(2001, 2009 + 1):
+        # docx
+        text = read_txt("TestSuite/JP Morgan/JP Morgan" + str(i) + "docx.txt")
+        # pdf
+        #text = read_txt("TestSuite/JP Morgan/JP Morgan" + str(i) + ".txt")
+        if text:
+            text = utf8_to_ascii(text)
+            tup = find_words(text.decode('unicode_escape').encode('ascii','ignore'))
+            print str(i) + ", " + str(tup[0]) + ", " + str(tup[1])
+
 
 def main():
-    #text = generate_random_string()
-    with open('TestSuite/BankOfNYMellon2012', 'r') as myfile:
-        text = myfile.read().decode("utf-8").replace('\n', '').lower()
-    #print text
-    count = count_words(text)
-    #print count
-    #count = stop_words(count)
-    #print len(count)
-    print count
-    print "Total # of words:    ", len(text.split())
-    print "# of words repeated: ", len(count)
-    print "# of 'we':           ", count['we']
-    print "# of 'our':          ", count['our']
+    get_jpm_op_th()
 
 if __name__ == "__main__":
     main()
