4bc4d590e962d0ee70fc3914270ff3f9ae9568dc
alvaro <delandaluce_alvaro@wheatoncollege.edu>
2017-02-16 16:15:15 -0500

    update opportunity and threat lists

--- word_count.py
+++ word_count.py
@@ -3,25 +3,63 @@ import re
 opportunity = ["advantage", "befalling", "break", "chance","connection",
                "contingency", "convenience", "cut", "fair shake", "favorable",
                "fighting chance", "fitness", "fling", "fortuity", "hope",
-               "luck", "recourse"]
+               "luck", "recourse", "advancement", "break", "chance", "favorable",
+               "fortune", "occasion", "opening", "possibility", "prospect",
+               "prosperity", "time", "turn", "stimulating", "successful",
+               "viable", "chance", "expect", "expectation", "expects", "expediency",
+               "favorability", "favorable", "luck", "momentum", "momentus", "opportune",
+               "opportunisitic", "opportunities", "opportunity", "optimism", "optimist",
+               "optimistic", "outlook", "prospects", "suitable", "upbeat", "advancement",
+                "break", "chance", "favorable", "fortune", "occasion", "opening",
+               "possibility", "prospect", "prosperity", "time", "turn", "stimulating",
+               "successful", "viable","advantage", "befalling", "break", "chance",
+               "connection", "contingency", "convenience", "cut", "fair shake", "favorable",
+               "fighting chance", "fitness", "fling", "fortuity", "hope", "luck", "recourse",
+               "chance", "expect", "expectation", "expects", "expediency", "favorability",
+               "favorable", "luck", "momentum", "momentus", "opportune", "opportunisitic",
+               "opportunities", "opportunity", "optimism", "optimist", "optimistic", "outlook",
+               "prospects", "suitable", "upbeat","advancement", "advantage","befalling",
+               "break", "chance", "connection", "contingency", "convenience", "cut", "expect",
+               "expectation", "expects", "expediency", "fair shake", "favorability", "favorable",
+               "fighting chance", "fitness", "fling", "fortuity", "fortune", "hope", "luck",
+               "momentum",  "momentous", "occasion", "opening", "opportune", "opportunistic",
+               "opportunities", "opportunity", "optimism", "optimist", "optimistic", "outlook",
+               "possibility", "prospect", "prospects", "prosperity", "prosperous", "recourse",
+               "stimulating", "successful", "suitable", "time", "turn", "upbeat", "viable"]
+
 threat = ["alarming fateful", "apocalyptic", "baleful", "baneful", "black",
           "cautionary", "challenging", "comminatory", "crisis", "dangerous",
           "demur", "dire", "grim", "ill-boding", "imminent", "impendent",
           "impending", "inauspicious", "intimidating", "looming", "loury",
           "lowering", "lowery", "minacious", "minatory", "portending", "portent",
           "sinister", "trial dire", "ugly", "unlucky", "unpropitious", "unsafe",
-          "warning",]
+          "warning","assault", "conflict", "constrained", "crisis", "crunch", "danger",
+          "foreboding", "hazard", "jeopardy", "loss", "menace", "negative", "omen", "peril",
+          "portent", "risk", "stressful", "sword of damocles", "urgent","assault", "conflict",
+          "constrained", "crisis", "crunch", "danger", "foreboding", "hazard", "jeopardy",
+          "loss", "menace", "negative", "omen", "peril", "portent", "risk", "stressful",
+          "sword of damocles", "urgent", "alarming", "apocalyptic", "baneful", "black",
+          "cautionary", "challenging", "comminatory", "crisis", "dangerous", "demur", "dire",
+          "grim", "ill-boding", "imminent", "impendent", "impending", "inauspicious",
+          "intimidating", "looming", "loury", "lowering", "lowery", "minacious", "minatory",
+          "portending", "portent", "sinister", "trial dire", "ugly", "unlucky", "unpropitious",
+          "unsafe", "warning", "fateful", "demur", "dire", "fateful", "foreboding", "grim",
+          "hazard", "ill-boding", "imminent", "impendent", "impending", "inauspicious",
+          "intimidating", "jeopardy", "looming", "loss", "loury", "lowering", "lowery", "menace",
+          "minacious", "minatory", "negative", "omen", "peril", "portending", "portent", "risk",
+          "sinister", "stressful", "sword of damocles", "trial dire", "ugly", "unlucky",
+          "unpropitious", "unsafe", "urgent", "warning"]
 
 enactment = ["aggrandizement", "amplif", "augment", "cultivate", "develop", "develops",
              "developing", "DILATION", "DISTENTION", "ENLARGE", "EXPAND", "EXPANDS", "EXPANSION",
              "EXTENSION", "GRADUALLY INCREASE", "GRADUAL INCREASE", "GROW", "GROWS", "GROWTH",
              "PLANTED", "PROGRESSION", "PROGRESS", "TURGESCENCE", "ACQUIRE", "ACQUISITION",
              "AMALGAMAT", "COALESCE", "INVESTMENT BANKER", "MERGE", "POOL", "PROCEEDS", "SYNDICATE",
-             "PURCHASE", "accomplish", "achieve", "act", "action", "active", "activity", "alacrity", 
+             "PURCHASE", "accomplish", "achieve", "act", "action", "active", "activity", "alacrity",
              "alertness", "battle", "combat", "commission", "conflict", "contest", "encounter", "engage",
              "engagement", "enterprise", "execute", "exercise", "exert", "exploit", "fight", "force",
              "influence", "intervene", "intervention", "maneuver", "manipulate", "move", "occupy",
-             "operate", "operation", "perform", "proceed", "process", "react", "respond", "rush", 
+             "operate", "operation", "perform", "proceed", "process", "react", "respond", "rush",
              "stir", "thrust", "transact", "undertake", "absorb", "affiliate", "ally", "assimilate",
              "blend", "cement", "centralize", "coalesce", "combine", "compound", "conglomerate",
              "consolidate", "converge", "fuse", "incorporate", "intermingle", "join", "league",
@@ -38,7 +76,26 @@ enactment = ["aggrandizement", "amplif", "augment", "cultivate", "develop", "dev
              "deconstruct", "disassemble", "disinherit", "dismantle", "divest", "divestiture", "forfeit",
              "oust", "relinquish", "remove", "sacrifice", "sell", "separate", "Environment",
              "environs", "industry", "market", "niche", "nook", "province", "purview", "region", "scope",
-             "sector", "terrain", "territory"]        
+             "sector", "terrain", "territory"]
+
+def scrub_list(lst):
+    for i in lst:
+        i = i.lower()
+    lst = list(sorted(set(lst)))
+    return lst
+
+
+print len(opportunity)
+opportunity = scrub_list(opportunity)
+print len(opportunity)
+print len(threat)
+threat = scrub_list(threat)
+print len(threat)
+print len(enactment)
+enactment = scrub_list(enactment)
+print len(enactment)
+
+
 
 # Delete useless words
 # input: Counter object
@@ -137,6 +194,9 @@ def get_jpm_op_th():
 
 def main():
     get_jpm_op_th()
+
+    pass
+
 
 if __name__ == "__main__":
     main()
