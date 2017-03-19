bcd820d8686fa283bbff4f4c2ad0f7256cc2777d
alvaro <delandaluce_alvaro@wheatoncollege.edu>
2017-03-19 13:49:02 -0400

    cleaning code

--- pdf_to_txt.py
+++ pdf_to_txt.py
@@ -4,15 +4,20 @@ import PyPDF2
 # input:  path to a pdf file (string)
 # return: file content in a string
 def pdf_to_txt(filepath):
-    pdf_file = open(filepath, 'rb')
-    read_pdf = PyPDF2.PdfFileReader(pdf_file)
-    number_of_pages = read_pdf.getNumPages()
-    page = read_pdf.getPage(10)
-    page_content = page.extractText()
-    return page_content.encode('utf-8')
+    try:
+        pdf_file = open(filepath, 'rb')
+        read_pdf = PyPDF2.PdfFileReader(pdf_file)
+        number_of_pages = read_pdf.getNumPages()
+        page_content = ""
+        for i in range(number_of_pages):
+            page = read_pdf.getPage(i)
+            page_content += page.extractText()
+        return page_content.encode('utf-8')
+    except IOError:
+        print "Could not read file: ", filepath
 
 def main():
-    txt = pdf_to_txt('DataFiles/ByCompany/Bank of NY Mellon/BYN Mellon 2012.pdf')
+    txt = pdf_to_txt('DataFiles/ByCompany/JP Morgan/JP Morgan 2001.pdf')
     print txt
 
 
