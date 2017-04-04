8775ae29aa1d7a5565cc837e1e2be6fdc2e27f79
Adnan Hassan <AdnanHassan@adnans-mbp.wheatonma.edu>
2017-04-04 11:23:32 -0400

    small fix

--- WordCount.py
+++ WordCount.py
@@ -147,9 +147,9 @@ class WordCount(object):
             self.scores.append(float(sum)/self.total_word_counts[index])
             self.sums.append(sum)
             index += 1
-        self.average_scores()
+        self.generate_averages()
 
-    def average_scores(self):
+    def generate_averages(self):
         self.average = []
         sum = 0   # total hardiness score
         sum2 = 0  # total word count score
